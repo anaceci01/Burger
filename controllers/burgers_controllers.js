ea55d87ad0b0e31d87e24d330cc185eec9210d5e
Ana Davis <anac.davis@gmail.com>
2020-01-16 00:01:10 -0700

    router post and router put on controllers updated

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,4 +1,30 @@
 const express = require("express");
+const burger = require("../models/burger");
+
 const router = express.Router();
-const burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hdbrsObj = {
+            burgers: data
+        };
+        console.log(hdbrsObj);
+        res.render("index", hdbrsObj);
+    });
+    router.post("/api/burgers", function(req, res) {
+        burger.instertOne(
+            ["burger_name", "devoured"], [req.body.burger_name, req.body.devoured],
+            function(result) {
+                //Send back he ID of the burger
+                res.json({ id: result.insertId });
+            }
+        );
+    });
+});
+
+
+
+
+
+
 module.exports = router;
