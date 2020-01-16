3c9a84cb5be5d620cc0ccc25a74de6c1f67e45b3
Ana Davis <anac.davis@gmail.com>
2020-01-15 23:23:39 -0700

    orm error fixed, it needed an extra bracket, and renamed functions

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,5 @@
-const connection = require("../config/connection");
-
+var connection = require("../config/connection");
+//
 function createQmarks(num) {
     var arr = [];
     for (var i = 0; i < num; i++) {
@@ -7,7 +7,7 @@ function createQmarks(num) {
     }
     return arr.toString();
 }
-
+// Create the string
 function translateSql(obj) {
     var arr = [];
     for (var key in ob) {
@@ -21,47 +21,46 @@ function translateSql(obj) {
     }
     return arr.toString();
 }
-const orm = {
-        selectAll: function(table, cb) {
-            let dbQuery = "SELECT * FROM" + table + ";";
-            connection.query(dbQuery, function(err, res) {
-                if (err) {
-                    throw err;
-                }
-                cb(res);
-            });
-        },
-        insertOne: function(table, cols, vals, cb) {
-            var dbQuery = "INSERT INTO " + table + "(" + cols.toString() + ")" + "VALUES(" + createQmarks(vals.length) + ")";
+var orm = {
+    selectAll: function(table, cb) {
+        var dbQuery = "SELECT * FROM" + table + ";";
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var dbQuery = "INSERT INTO " + table + "(" + cols.toString() + ")" + "VALUES (" + createQmarks(vals.length) + ")";
 
-            console.log(dbQuery);
-            connection.query(dbQuery, vals, function(err, res) {
-                if (err) {
-                    throw err;
-                }
-                cb(res);
-            });
-        },
-        updateOne: function(table, objColVals, condition, cb) {
-            var dbQuery = "UPDATE" + table + "SET" + translateSql(objColVals) + "WHERE" + condition;
-            console.log(dbQuery);
-            connection.query(dbQuery, function(err, res) {
-                if (err) {
-                    throw err;
-                }
-                cb(res);
-            });
-        },
-        deleteOne: function(table, condition, cb) {
-            var dbQuery = "DELETE FROM" + table + "WHERE" + condition;
-            console.log(dbQuery);
+        console.log(dbQuery);
+        connection.query(dbQuery, vals, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var dbQuery = "UPDATE" + table + "SET" + translateSql(objColVals) + "WHERE" + condition;
+        console.log(dbQuery);
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    deleteOne: function(table, condition, cb) {
+        var dbQuery = "DELETE FROM" + table + "WHERE" + condition;
+        console.log(dbQuery);
 
-            connection.query(dbQuery, function(err, res) {
-                if (err) {
-                    throw err;
-                }
-                cb(res);
-            });
-        }
-
-            module.exports = orm
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+};
