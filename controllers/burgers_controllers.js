c67ac0ea662dae6cd052222f7ef5de0285414bbc
Ana Davis <anac.davis@gmail.com>
2020-01-19 01:41:12 -0700

    re-routed functions and variables, it is working nowgit add

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -5,33 +5,35 @@ var router = express.Router();
 
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
-        var hdbrsObj = {
-            burgers: data
-        };
+        res.render("index", { burger: data });
+    })
+});
+
+router.get("/api/burgers", function(req, res) {
+    burger.selectAll(function(data) {
         console.log(data);
         res.json(data);
     });
 });
+
 router.post("/api/burgers", function(req, res) {
-    burger.instertOne(
-        ["burger_name", "devoured"], [req.body.burger_name, req.body.devoured],
-        function(result) {
-            //Send back he ID of the burger
-            res.json({ id: result.insertId });
-        }
-    )
+    if (req.body.burger_name === undefined || req.body.devoured === undefined || req.body.burger_name.length < 1 || req.body.burger_name.length > 30) {
+        return res.status(400).end();
+    }
+    burger.insertOne(req.body.burger_name, req.body.devoured, function(err, data) {
+        if (err) return res.status(400).end();
+        res.json({ id: data.insertId });
+    });
 });
-router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    console.log("condition", condition);
-    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
-        if (result, changedRows === 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    })
+
+router.put("/api/burgers/:id ", function(req, res) {
+    let hdbrsObj = {};
+    req.body.burger_name ? hdbrsObj["burger_name"] = req.body.burger_name : null;
+    if (req.body.devoured != undefined) {
+        req.body.devoured === "true" ? hdbrsObj["devoured"] = true : hdbrsObj["devoured"] = false;
+    };
 });
+
 router.delete("/api/burgers/:id", function(req, res) {
     burger.deleteOne(req.params.id, function(err, data) {
         if (err) return res.status(400).end();
